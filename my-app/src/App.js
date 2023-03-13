be52b39a0754a2b6a16d5e9a65caed8d09abdc20
Krishna <85607892+Krishna4537@users.noreply.github.com>
2023-03-13 12:17:05 +0100

    basic structure,Added Routing and bottom nav

---
+++ my-app/src/App.js
@@ -0,0 +1,35 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { NavLink, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import SimpleBottomNavigation from "./components/MainNav";
+import { Container } from "@mui/material";
+
+//pages
+import Home from "./Pages/Home/Home";
+import Analytics from "./Pages/Analytics/Analytics";
+import Discover from "./Pages/Discover/Discover";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="app">
+        <Container>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/analytics" element={<Analytics />} />
+            <Route path="/discover" element={<Discover />} />
+            {/* <Route path="/search" component={Search} /> */}
+          </Routes>
+
+          <h1>subscribe</h1>
+        </Container>
+      </div>
+
+      <SimpleBottomNavigation />
+    </>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/MainNav.js
@@ -0,0 +1,75 @@
+import { useEffect } from "react";
+import Box from "@mui/material/Box";
+import { styled } from "@mui/material/styles";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import HomeIcon from "@mui/icons-material/Home";
+import InsertChartIcon from "@mui/icons-material/InsertChart";
+import SearchIcon from "@mui/icons-material/Search";
+import WhatshotIcon from "@mui/icons-material/Whatshot";
+import { useNavigate, Outlet } from "react-router-dom";
+
+const RootBox = styled(Box)({
+  width: "100%",
+  position: "fixed",
+  bottom: 0,
+  backgroundColor: "#2d313a",
+  zIndex: 100,
+});
+
+const useStyles = () => ({
+  label: {
+    color: "white",
+  },
+});
+
+export default function SimpleBottomNavigation() {
+  const classes = useStyles();
+  const navigate = useNavigate();
+
+  const handleNavigation = (event, newValue) => {
+    if (newValue === 0) {
+      navigate("/");
+    } else if (newValue === 1) {
+      navigate("/analytics");
+    } else if (newValue === 2) {
+      navigate("/discover");
+    } else if (newValue === 3) {
+      navigate("/search");
+    }
+  };
+
+  useEffect(() => {
+    // Reset the value of the BottomNavigation when the route changes
+    handleNavigation(null, 0);
+  }, []);
+
+  return (
+    <RootBox>
+      <BottomNavigation value={0} onChange={handleNavigation} showLabels>
+        <BottomNavigationAction
+          classes={{ label: classes.label }}
+          label="Home"
+          icon={<HomeIcon />}
+        />
+        <BottomNavigationAction
+          classes={{ label: classes.label }}
+          label="Analytics"
+          icon={<InsertChartIcon />}
+        />
+        <BottomNavigationAction
+          classes={{ label: classes.label }}
+          label="Discover"
+          icon={<WhatshotIcon />}
+        />
+        <BottomNavigationAction
+          classes={{ label: classes.label }}
+          label="Search"
+          icon={<SearchIcon />}
+        />
+      </BottomNavigation>
+      <Outlet />
+    </RootBox>
+  );
+}
